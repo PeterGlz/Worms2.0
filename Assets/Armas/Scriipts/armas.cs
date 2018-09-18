ec17f2e53d22b11623a9fe8e5195156642a40775
JosZarate <pepe-ds-pokes@hotmail.com>
2018-09-17 23:33:06 -0500

    armamento0.03

--- Assets/Armas/Scriipts/armas.cs
+++ Assets/Armas/Scriipts/armas.cs
@@ -16,15 +16,37 @@ public class armas : MonoBehaviour
 	
 	void Update ()
     {
-		switch(armaI)
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            armaI = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            armaI = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            armaI = 3;
+        }
+
+        switch (armaI)
         {
             case 1: //tenedor
+                arma1.SetActive(true);
+                arma2.SetActive(false);
+                arma3.SetActive(false);
                 break;
 
             case 2: //metralleta
+                arma1.SetActive(false);
+                arma2.SetActive(true);
+                arma3.SetActive(false);
                 break;
 
             case 3: //mina
+                arma1.SetActive(false);
+                arma2.SetActive(false);
+                arma3.SetActive(true);
                 break;
         }
 	}
