420eeaab204bbb2055c948bbb06f93872d35e534
JosZarate <pepe-ds-pokes@hotmail.com>
2018-09-26 23:26:48 -0500

    algo

---
+++ Assets/Armas/Scriipts/minaF.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minaF : MonoBehaviour
+{
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Destroy(gameObject, 4);
+	}
+}
