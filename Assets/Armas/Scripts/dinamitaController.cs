31c0d22b70dd8d2b9dda09c30ac60d4472acf87d
JosZarate <pepe-ds-pokes@hotmail.com>
2018-10-21 23:37:43 -0500

    más armas

---
+++ Assets/Armas/Scripts/dinamitaController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dinamitaController : MonoBehaviour
+{
+    private armas usarDinamita;
+
+    void Start()
+    {
+        usarDinamita = FindObjectOfType<armas>();
+        StartCoroutine(Explosion());
+    }
+
+    IEnumerator Explosion()
+    {
+        yield return new WaitForSeconds(4);
+        usarDinamita.objSuelo = false;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Armas/Scripts/granadaController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class granadaController : MonoBehaviour
+{
+    private armas usarGranada;
+    public float vel;
+    [SerializeField] private Vector3 direction = Vector3.right;
+
+    void Start ()
+    {
+        usarGranada = FindObjectOfType<armas>();
+        StartCoroutine(Explosion());
+    }
+	
+	void Update ()
+    {
+        transform.Translate(direction * vel * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Piso")
+        {
+            vel = 0;
+        }
+    }
+
+    IEnumerator Explosion()
+    {
+        yield return new WaitForSeconds(4);
+        usarGranada.objSuelo = false;
+        Destroy(gameObject);
+    }
+}
