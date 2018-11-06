450cfc4b84a809275586ed53e2f3a9511c5a2500
Israel <israelelizalde@hotmail.com>
2018-11-06 10:31:03 -0600

    Mapa

---
+++ Assets/Mapa/Generar.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generar : MonoBehaviour {
+
+
+    Vector2 vec;
+    public GameObject[][] mapa;
+    public int x;
+    public int y;
+    public GameObject[] esquina_izq;
+    public GameObject[] esquina_der;
+    public GameObject[] arriba;
+    public GameObject[] izq;
+    public GameObject[] der;
+    public GameObject[] centro;
+
+
+
+    void Start () {
+
+        
+
+
+
+        vec = new Vector2(x, y);
+       mapa = new GameObject[(int)vec.x][];
+
+        for(int i = 0; i <= x-1;i++)
+        {
+            
+            for (int j = 0; j <= y-1;j++)
+            {
+                mapa[i] = new GameObject[(int)vec.y];
+                if (i == 0 && j==0)
+                {
+                    mapa[i][j] = esquina_izq[Random.Range(0, esquina_izq.Length)];
+
+                }
+                if(i == 0 && j > 0 && j < y-1)
+                {
+                    mapa[i][j] = arriba[Random.Range(0, arriba.Length)];
+                }
+                if(i == 0 && j== y-1)
+                {
+                    mapa[i][j] = esquina_der[Random.Range(0, esquina_der.Length)];
+                }
+
+
+                if(x>1  && j == 0 && i >0)
+                {
+                    mapa[i][j] = izq[Random.Range(0, izq.Length)];
+                }
+
+                if(i>0&& j > 0 && j < y-1)
+                {
+                    mapa[i][j] = centro[Random.Range(0, centro.Length)];
+                }
+                if(i>0&& j == y-1)
+                {
+                    mapa[i][j] = der[Random.Range(0, der.Length)];
+                }
+
+
+
+                Instantiate(mapa[i][j], new Vector3(j, -i, 0), Quaternion.identity);
+
+            }
+        }
+
+       // mapa[0][0] = esquina_izq[Random.Range(0, esquina_izq.Length)];
+        //Instantiate(mapa[0][0], new Vector3(0, 0, 0), Quaternion.identity);
+
+    }
+	
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Mapa/randomInstante.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomInstante : MonoBehaviour {
+
+    public GameObject[] m_prefabs;
+    public int m_amount;
+
+    public Transform m_left, m_right;
+
+    public float m_WaitTime = 0.01f;
+
+    private bool m_isInstantitating;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!m_isInstantitating)
+            {
+                StartCoroutine(RandomInstantiate());
+            }
+        }
+	}
+
+    IEnumerator RandomInstantiate()
+    {
+        m_isInstantitating = true;
+        for(int i=0;i<m_amount;i++)
+        {
+            Instantiate(m_prefabs[Random.Range(0, m_prefabs.Length)], new Vector3(Random.Range(m_left.position.x, m_right.position.x), transform.position.y, transform.position.z), Random.rotation);
+            yield return new WaitForSeconds(m_WaitTime);
+
+        }
+        m_isInstantitating = false;
+    }
+}
